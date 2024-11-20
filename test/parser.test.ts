462a7b427eb239021070188e19a5d3579f9118bf
Amanda Randombage <74random.amanda@gmail.com>
2024-11-20 11:20:39 +0200

    Fix Parser tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -6,17 +6,29 @@ var assert = chai.assert;
 
 describe('Test for Parser module', () => {
 
-    it('Get subdomain test', () => {
+    it('Get Subdomain test', () => {
         assert.equal(parser.getSubDomain("sub.example.com") , 'sub');
     });
-    it('Get domain test', () => {
-        assert.equal(parser.getHostname("sub.example.com") , 'sub');
+    it('Get Domain test', () => {
+        assert.equal(parser.getDomain("sub.example.com") , 'example.com');
+    });
+    it('Get Domain without Suffix test', () => {
+        assert.equal(parser.getDomainWithoutSuffix("sub.example.com") , 'example');
+    });
+    it('Get Public Suffix test', () => {
+        assert.equal(parser.getPublicSuffix("sub.example.com") , 'com');
     });
     it('Is ICAAN test', () => {
-        assert.equal(parser.getHostname("sub.example.com") , 'sub');
+        assert.equal(parser.isIcann("sub.example.com") , true);
     });
-    it('Get domain test', () => {
-        assert.equal(parser.getHostname("sub.example.com") , 'sub');
+    it('Is IP test', () => {
+        assert.equal(parser.isIp("sub.example.com") , false);
+    });
+    it('Is Private test', () => {
+        assert.equal(parser.isPrivate("sub.example.com") , false);
+    });
+    it('Get Hostname test', () => {
+        assert.equal(parser.getHostname("sub.example.com") , 'sub.example.com');
     });
 
 });
