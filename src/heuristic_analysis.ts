3b01475b2f622c0d1794d060fb04c747adbcda06
Chau Nguyen <6055140+kinggreedy@users.noreply.github.com>
2024-12-01 10:06:42 +0000

    format code

--- src/heuristic_analysis.ts
+++ src/heuristic_analysis.ts
@@ -3,74 +3,74 @@
 
 TODO: Load list of domains from can-i-take-over-xyz fingerprints.json */
 
-import * as listOfDomains from "./fingerprints.json";
-import axios from "axios";
-import ping from "ping";
+import * as listOfDomains from './fingerprints.json';
+import axios from 'axios';
+import ping from 'ping';
 
 function seekDomain(cname: string[], domain: string) {
-    if (cname.length == 0){
-        return 0;
-    }
-
-    //TODO: Needs some other way to crawl through the list. Now loops through the full list without breaking when a positive result found. I tried returning out of the function, but the forEach function loop eats the return.
-    for (const name of cname) {
-        if (domain.indexOf("." + name) > -1) {
-            return 1;
-        }
-    }
-
+  if (cname.length == 0) {
     return 0;
+  }
+
+  //TODO: Needs some other way to crawl through the list. Now loops through the full list without breaking when a positive result found. I tried returning out of the function, but the forEach function loop eats the return.
+  for (const name of cname) {
+    if (domain.indexOf('.' + name) > -1) {
+      return 1;
+    }
+  }
+
+  return 0;
 }
 
 export function matchDomain(domain: string) {
-    let parsedDomain: string;
-    try {
-        parsedDomain = new URL("https://" + domain).hostname;
-        if (!parsedDomain || parsedDomain.split('.').length < 2) {
-            return -1;
-        }
-    } catch (error) {
-        return -1;
+  let parsedDomain: string;
+  try {
+    parsedDomain = new URL('https://' + domain).hostname;
+    if (!parsedDomain || parsedDomain.split('.').length < 2) {
+      return -1;
     }
+  } catch (error) {
+    return -1;
+  }
 
-    for (const d of listOfDomains) {
-        if (seekDomain(d.cname, parsedDomain) === 1 && d.status === "Vulnerable") {
-            return 1;
-        }
+  for (const d of listOfDomains) {
+    if (seekDomain(d.cname, parsedDomain) === 1 && d.status === 'Vulnerable') {
+      return 1;
     }
+  }
 
-    return 0;
+  return 0;
 }
 
 export async function checkForWebServer(protocol: string, domain: string) {
-    /* Checks if there is a web server answering from a given domain. */
-    const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-    let value: number = -1;
-    const url = `${protocol}://${domain}`;
+  /* Checks if there is a web server answering from a given domain. */
+  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  let value: number = -1;
+  const url = `${protocol}://${domain}`;
 
-    try {
-        const response = await axios.get(url);
-        // await delay(1950);
-        value = response.status <= 500 ? 1 : 0;
-    } catch (error) {
-        value = 0;
-    } finally {
-        //console.log(value);
-        return value;
-    }
+  try {
+    const response = await axios.get(url);
+    // await delay(1950);
+    value = response.status <= 500 ? 1 : 0;
+  } catch (error) {
+    value = 0;
+  } finally {
+    //console.log(value);
+    return value;
+  }
 }
 
 export async function pingServer(server: string) {
-    // Function from: https://medium.com/@abhipillai/how-to-ping-ip-address-using-typescript-565105b7f6fa
-    try {
-        const response = await ping.promise.probe(server);
-        if (response.alive) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        console.error("Error in pinging: " + error);
-        return false;
+  // Function from: https://medium.com/@abhipillai/how-to-ping-ip-address-using-typescript-565105b7f6fa
+  try {
+    const response = await ping.promise.probe(server);
+    if (response.alive) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    console.error('Error in pinging: ' + error);
+    return false;
+  }
 }
